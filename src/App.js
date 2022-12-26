ee09a83376ebe3857b0b2ad1f3f64e80f16c5e43
Emon Li <emonli.328@gmail.com>
2022-12-26 14:06:19 -0500

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import Header from './components/Header'
+import AboutMe from './components/AboutMe'
 import './App.css'
 
 const App = () => {
   return (
     <>
       <Header />
+      <AboutMe />
+
+      <div className="skills">
+        <div className="container">
+
+        <h3>React</h3>
+        <h3>HTML 5</h3>
+        <h3>CSS</h3>
+        <h3>JavaScript</h3>
+        <h3>Python</h3>
+        <h3>jQuery</h3>
+        <h3>Express</h3>
+        <h3>Heroku</h3>
+        <h3>Django</h3>
+        <h3>Mongo DB</h3>
+        <h3>SQL</h3>
+        <h3>Node.js</h3>
+        </div>
+      </div>
     
-
-    <div className="about-me-container">
-
-    
-      <h1>About Me.</h1>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-    </div>
-
 
     </>
   );
