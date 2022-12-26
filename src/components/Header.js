d8a63d739d0dcf12164ad38ffef72a24faf4f6b9
Emon Li <emonli.328@gmail.com>
2022-12-26 13:42:40 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+
+        <h1>Emon.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
