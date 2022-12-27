cb028b561090bdb5e9334345d35bad0db06b7c0c
Emon Li <emonli.328@gmail.com>
2022-12-26 22:51:03 -0500

    added context to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
     <div className="header">
       <div className="container">
 
-        <h1>Emon.</h1>
+        <h1>Hello! I'm Emon!</h1>
+        <h3>I'm a CrossFit enthusiast and an avid coffee drinker.</h3>
+        
       </div>
     </div>
   )
