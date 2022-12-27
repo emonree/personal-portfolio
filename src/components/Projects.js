d6ac6c3e6d971af4f3cbe7f01575f2c8623db024
Emon Li <emonli.328@gmail.com>
2022-12-26 22:19:53 -0500

    created Skills and Projects components

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Projects.css'
+
+const Projects = () => {
+  return (
+    <div className="Projects">
+      <div className="container">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Projects;
---
+++ src/components/Skills.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <div className="skills">
+        <div className="container">
+        <h3>React</h3>
+        <h3>HTML 5</h3>
+        <h3>CSS</h3>
+        <h3>JavaScript</h3>
+        <h3>Python</h3>
+        <h3>jQuery</h3>
+        <h3>Express</h3>
+        <h3>Heroku</h3>
+        <h3>Django</h3>
+        <h3>Mongo DB</h3>
+        <h3>SQL</h3>
+        <h3>Node.js</h3>
+        </div>
+      </div>
+  )
+}
+
+export default Skills;
