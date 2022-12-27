9ee6808f5c02c235cb377a0bc71a74f4dec467ad
Emon Li <emonli.328@gmail.com>
2022-12-26 22:19:35 -0500

    rendered components into JSX

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import Header from './components/Header'
 import AboutMe from './components/AboutMe'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
 import './App.css'
 
 const App = () => {
   return (
     <>
+
       <Header />
       <AboutMe />
+      <Skills />
+      <Projects />
 
-      <div className="skills">
-        <div className="container">
-
-        <h3>React</h3>
-        <h3>HTML 5</h3>
-        <h3>CSS</h3>
-        <h3>JavaScript</h3>
-        <h3>Python</h3>
-        <h3>jQuery</h3>
-        <h3>Express</h3>
-        <h3>Heroku</h3>
-        <h3>Django</h3>
-        <h3>Mongo DB</h3>
-        <h3>SQL</h3>
-        <h3>Node.js</h3>
-        </div>
-      </div>
     
 
     </>
