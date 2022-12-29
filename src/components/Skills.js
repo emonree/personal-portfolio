73afbc5a2d619941a76a2c79b93f0bfdf94a5282
Emon Li <emonli.328@gmail.com>
2022-12-29 12:33:19 -0500

    added logos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './Skills.css'
-import { RiReactjsLine } from 'react-icons/ri';
+import { RiReactjsLine } from 'react-icons/ri'
+import { GrHtml5 } from 'react-icons/gr'
+import { IoLogoCss3, IoLogoJavascript, IoLogoPython, IoLogoNodejs } from 'react-icons/io'
+import { DiJqueryLogo, DiDjango } from 'react-icons/di'
+import { SiHeroku, SiMongodb, SiPostgresql } from 'react-icons/si'
 
 const Skills = () => {
   return (
     <div className="skills" id="skills">
         <div className="container">
-          <RiReactjsLine size="8em"/>
-        <h3>React</h3>
-        <h3>HTML 5</h3>
-        <h3>CSS</h3>
-        <h3>JavaScript</h3>
-        <h3>Python</h3>
-        <h3>jQuery</h3>
-        <h3>Express</h3>
-        <h3>Heroku</h3>
-        <h3>Django</h3>
-        <h3>Mongo DB</h3>
-        <h3>SQL</h3>
-        <h3>Node.js</h3>
+          <RiReactjsLine size="8em" className="icons"/>
+          <GrHtml5 size="7.5em" className="icons"/>
+          <IoLogoCss3 size="7.5rem" className="icons"/>
+          <IoLogoJavascript size="8rem" className="icons"/>
+          <IoLogoPython size="8rem" className="icons"/>
+          <DiJqueryLogo size="8rem" className="icons"/>
+          <IoLogoNodejs size="7.5rem" className="icons"/>
+          <SiHeroku size="7rem" className="icons"/>
+          <DiDjango size="8rem" className="icons"/>
+          <SiMongodb size="7.5rem" className="icons"/>
+          <SiPostgresql size="7.5rem" className="icons"/>
         </div>
       </div>
   )
