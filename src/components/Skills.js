c3dee94aae7209d1ecd8eef1a00095d1072c2d6e
Emon Li <emonli.328@gmail.com>
2022-12-29 15:05:59 -0500

    styling to icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,77 @@ const Skills = () => {
       <div className="container">
         <h1 className="skills-header">Some technologies I use</h1>
         <div className="icons-container">
-        <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            />
+            <h3 className="skills-name">React</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+            />
+            <h3 className="skills-name">HTML5</h3>
+          </div>
+
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+            />
+            <h3 className="skills-name">CSS3</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            />
+            <h3 className="skills-name">JavaScript</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+            />
+            <h3 className="skills-name">Python</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+            />
+            <h3 className="skills-name">Node.js</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg"
+            />
+            <h3 className="skills-name">Django</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+            />
+            <h3 className="skills-name">Mongo DB</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+            />
+            <h3 className="skills-name">PostgreSQL</h3>
+          </div>
+          <div className="hvr-float-shadow">
+            <img
+              className="icons"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+            />
+            <h3 className="skills-name">VS Code</h3>
+          </div>
         </div>
       </div>
     </div>
