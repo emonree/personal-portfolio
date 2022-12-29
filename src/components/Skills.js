ca103882865b8b1140a22fbbe7630ceaaa31a184
Emon Li <emonli.328@gmail.com>
2022-12-29 14:14:50 -0500

    replaced react-icons with dev icns for logos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,5 @@
 import React from "react";
 import "./Skills.css";
-import { RiReactjsLine } from "react-icons/ri";
-import {
-  IoLogoHtml5,
-  IoLogoCss3,
-  IoLogoJavascript,
-  IoLogoPython,
-  IoLogoNodejs,
-} from "react-icons/io";
-import { DiDjango } from "react-icons/di";
-import { SiMongodb, SiPostgresql } from "react-icons/si";
-import { VscCode } from "react-icons/vsc";
 
 const Skills = () => {
   return (
@@ -18,16 +7,16 @@ const Skills = () => {
       <div className="container">
         <h1 className="skills-header">Some technologies I use</h1>
         <div className="icons-container">
-          <RiReactjsLine size="8em" className="icons" />
-          <IoLogoHtml5 size="7.5em" className="icons" />
-          <IoLogoCss3 size="7.5rem" className="icons" />
-          <IoLogoJavascript size="8rem" className="icons" />
-          <IoLogoPython size="8rem" className="icons" />
-          <IoLogoNodejs size="7.5rem" className="icons" />
-          <DiDjango size="8rem" className="icons" />
-          <SiMongodb size="7.5rem" className="icons" />
-          <SiPostgresql size="7.5rem" className="icons" />
-          <VscCode size="8rem" className="icons" />
+        <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+          <img className="icons" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
         </div>
       </div>
     </div>
