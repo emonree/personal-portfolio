6343fcb1f3212dcb30c741420c55d16ae2c4d82e
Emon Li <emonli.328@gmail.com>
2023-01-10 15:43:32 -0500

    adding content and styling to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Projects.css";
+import PlantNurseryImage from '../assets/plantnursery.png';
 
 const Projects = () => {
   return (
@@ -7,6 +8,44 @@ const Projects = () => {
       <div className="container">
         <div className="projects-header">
           <h1>Projects</h1>
+          <div className="project-container">
+            <div className="plant-nursery-project">
+              <a
+                className="hvr-float-shadow"
+                href="https://sunshine-nursery.netlify.app/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img
+                  className="plant-nursery-img"
+                  src={PlantNurseryImage}
+                ></img>
+              </a>
+              <p className="plant-project-info">
+                If you love plants like we do, this app showcases the different
+                types of plants and provides information on what climate they
+                thrive best in, the amount of light and water required, and the
+                plant hardiness zone to keep your plants growing. A
+                collaboration project with{" "}
+                <a
+                  className="bailey-link"
+                  href="https://github.com/texasmami/plant-nursery-front-end"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Bailey
+                </a>
+                . An app built with React, Express, and Mongoose.
+              </p>
+            </div>
+            <div className="first-portfolio-project">
+              <a
+                href="https://clinquant-duckanoo-46dc05.netlify.app/index.html"
+                target="_blank"
+                rel="noreferrer noopener"
+              ></a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
