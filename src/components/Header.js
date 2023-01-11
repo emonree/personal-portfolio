0f8c3d2e7d1a5263146e67e9d93bbeeac9277520
Emon Li <emonli.328@gmail.com>
2023-01-11 11:33:10 -0500

    styling nav and social icons for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -123,16 +123,60 @@ const Header = () => {
             </label>
             <ul class="menu">
               <li>
-                <a href="aboutme.html">About Me</a>
+                <a href="#about-me">About Me</a>
               </li>
               <li>
-                <a href="resume.html">Work Experience</a>
+                <a href="#skills">Toolbox</a>
               </li>
               <li>
-                <a href="projects.html">Portfolio</a>
+                <a href="#projects">Portfolio</a>
               </li>
-              <li>
-                <a href="travel.html">Travels</a>
+              <li className="li-social-icons">
+                <div className="social-icons">
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/emonli/"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      <img
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                        width="40px"
+                        alt="github"
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/emonree"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      <img
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg"
+                        width="40px"
+                        alt="linkedin"
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span className="resume">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="38.5"
+                          height="38"
+                          fill="currentColor"
+                          class="bi bi-person-workspace"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                          <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
+                        </svg>
+                      </span>
+                    </a>
+                  </li>
+                </div>
               </li>
             </ul>
           </section>
